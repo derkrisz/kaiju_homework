5034664cdd4b47497ba445de04a22069ec0054f8
derkrisz <krisztian.dergmail.com>
2017-12-11 18:10:25 +0000

    sets up Kaiju abstract class

--- src/main/java/Kaiju/Kaiju.java
+++ src/main/java/Kaiju/Kaiju.java
@@ -1,4 +1,14 @@
 package Kaiju;
 
-public class Kaiju {
+public abstract class Kaiju {
+
+    private String name;
+    private int healthValue;
+    private int attackValue;
+
+    public Kaiju(String name, int healthValue, int attackValue){
+        this.name = name;
+        this.healthValue = healthValue;
+        this.attackValue = attackValue;
+    }
 }
