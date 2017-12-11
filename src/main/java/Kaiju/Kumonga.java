6993feeed7811ba6ac121a62204ca5dea08105db
derkrisz <krisztian.dergmail.com>
2017-12-11 18:13:39 +0000

    creates two kaiju monsters as subclasses

---
+++ src/main/java/Kaiju/Kumonga.java
@@ -0,0 +1,7 @@
+package Kaiju;
+
+public class Kumonga extends Kaiju {
+    public Kumonga(String name, int healthValue, int attackValue) {
+        super(name, healthValue, attackValue);
+    }
+}
---
+++ src/main/java/Kaiju/Mothra.java
@@ -0,0 +1,7 @@
+package Kaiju;
+
+public class Mothra extends Kaiju {
+    public Mothra(String name, int healthValue, int attackValue) {
+        super(name, healthValue, attackValue);
+    }
+}
