6ca3b5bab32c5a52fee3f736b91389ef45d07368
derkrisz <krisztian.dergmail.com>
2017-12-11 18:47:07 +0000

    sets up vehicle superclass

--- src/main/java/Vehicle/Vehicle.java
+++ src/main/java/Vehicle/Vehicle.java
@@ -1,4 +1,13 @@
 package Vehicle;
 
 public class Vehicle {
+
+    private String type;
+    private int healthValue;
+
+    public Vehicle(String type, int healthValue){
+        this.type = type;
+        this.healthValue = healthValue;
+    }
+
 }
